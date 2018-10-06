043650234ba0efdab6a4275f3ab380a4cab98526
Nicholas Singh <nicholas1singh@gmail.com>
2018-10-06 11:29:41 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,1 +1,10 @@
-# squares
+class Cell:
+  def __init__(self,x,y,p):
+    self.p = p
+    self.y = y
+    self.x = x
+  def add_piece(self,piece):
+    self.p = Piece(p)
+  def remove_piece(self):
+    self.p = Piece(piece)  
+    
