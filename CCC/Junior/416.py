cf1c3ce03a875fd12b3a02d1db675360569a951a
DestroyedEpisode <43649472+DestroyedEpisode@users.noreply.github.com>
2018-11-20 19:16:21 -0500

    Update 416.py

--- CCC/Junior/416.py
+++ CCC/Junior/416.py
@@ -1,9 +1,11 @@
 # https://dmoj.ca/problem/mockccc15j1
 a=input().split()
 if int(a[0])<1000:
-  if a[0]=='416' or a[0]=='437':
-    print("Valuable")
+  if a[0]=='416':
+    print("valuable")
+  elif a[0]=='647' or a[0]=='437':
+    print("valueless")
   else:
-    print("Invalid")
+    print("invalid")
 else:
-  print("Invalid")
+  print("invalid")
