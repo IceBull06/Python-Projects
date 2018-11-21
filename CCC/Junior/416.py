d56d4d8f2bb5e92e63a7e0342154cd283c3525b9
DestroyedEpisode <43649472+DestroyedEpisode@users.noreply.github.com>
2018-11-20 19:10:23 -0500

    Update 416.py

--- CCC/Junior/416.py
+++ CCC/Junior/416.py
@@ -1,1 +1,9 @@
 # https://dmoj.ca/problem/mockccc15j1
+a=input().split()
+if a<1000:
+  if int(a[0])==416 or int(a[0])==437:
+    print("Valuable")
+  else:
+    print("Invalid")
+else:
+print("Invalid")
